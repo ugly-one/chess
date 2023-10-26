c62794d2a8ac999c59b7a8d383d641f80218fa9c
UglyOne <omojzboze@protonmail.com>
2023-10-26 13:16:41 +0200

    clean-up

--- board.gd
+++ board.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-
 	# add black pieces
 	add_piece("res://black_rook.svg", 0, 0)
 	add_piece("res://black_knight.svg", 0, 1)
@@ -26,8 +24,6 @@ func _ready():
 	add_piece("res://white_rook.svg", 7, 7)
 	for n in 8:
 		add_piece("res://white_pawn.svg", 6, n)
-		
-	pass # Replace with function body.
 
 func add_piece(texture_path, y, x):
 	var piece = load("res://piece.tscn").instantiate()
@@ -39,17 +35,6 @@ func add_piece(texture_path, y, x):
 	piece.moved.connect(on_piece_moved)
 	add_child(piece)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func place(piece, position):
-	var newPosition = get_centered_position(position)
-	if (newPosition == Vector2.ZERO):
-		piece.position = piece.start_pos
-	else:
-		piece.position = newPosition
-
 func get_centered_position(position: Vector2) -> Vector2:
 	var column = int(position.x) / 40
 	var row = int(position.y) / 40
@@ -58,4 +43,8 @@ func get_centered_position(position: Vector2) -> Vector2:
 	return Vector2(column * 40 + 20, row * 40 + 20)
 	
 func on_piece_moved(piece, position):
-	place(piece, position)
+	var newPosition = get_centered_position(position)
+	if (newPosition == Vector2.ZERO):
+		piece.position = piece.start_pos
+	else:
+		piece.position = newPosition
