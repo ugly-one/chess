ad1d8cec81d95c9e48e025e683c1b23f57baed06
UglyOne <omojzboze@protonmail.com>
2023-10-29 13:30:47 +0100

    trying to make pieces work correctly

--- board.gd
+++ board.gd
@@ -1,34 +1,16 @@
 extends Node2D
 
-func _ready():
-	# add black pieces
-	add_piece("res://black_rook.svg", 0, 0)
-	add_piece("res://black_knight.svg", 0, 1)
-	add_piece("res://black_bishop.svg", 0, 2)
-	add_piece("res://black_queen.svg", 0, 3)
-	add_piece("res://black_king.svg", 0, 4)
-	add_piece("res://black_bishop.svg", 0, 5)
-	add_piece("res://black_knight.svg", 0, 6)
-	add_piece("res://black_rook.svg", 0, 7)
-	for n in 8:
-		add_piece("res://black_pawn.svg", 1, n)
-		
-	# add white pieces
-	add_piece("res://white_rook.svg", 7, 0)
-	add_piece("res://white_knight.svg", 7, 1)
-	add_piece("res://white_bishop.svg", 7, 2)
-	add_piece("res://white_queen.svg", 7, 3)
-	add_piece("res://white_king.svg", 7, 4)
-	add_piece("res://white_bishop.svg", 7, 5)
-	add_piece("res://white_knight.svg", 7, 6)
-	add_piece("res://white_rook.svg", 7, 7)
-	for n in 8:
-		add_piece("res://white_pawn.svg", 6, n)
+var startingPosition
 
-func add_piece(texture_path, y, x):
-	var piece = load("res://piece.tscn").instantiate()
-	var texture = load(texture_path)
-	piece.set_texture(texture)
-	piece.set_chess_position(x, y)
-	
-	add_child(piece)
+func _on_piece_dropped(piece: Node2D):
+	# make sure the piece is corrently placed within the board
+	var x: int = piece.position.x / 40
+	var y: int = piece.position.y / 40
+	if (x < 0 || x > 7 || y < 0 || y > 7):
+		piece.position = startingPosition
+		return
+	piece.position.x = x * 40 + 20
+	piece.position.y = y * 40 + 20
+
+func _on_piece_lifted(piece: Node2D):
+	startingPosition = piece.position
--- piece.gd
+++ piece.gd
@@ -1,54 +1,31 @@
-extends Sprite2D
+extends StaticBody2D
 
-signal moved
+signal dropped
+signal lifted
 
-var isMoving = false
-var fieldSize = 40
-var start_pos
+var can_drag = false
+var dragging = false
+
 
 func _input(event):
-	
-	if (event is InputEventMouseButton 
-		and event.is_released() 
-		and event.button_index == MOUSE_BUTTON_LEFT
-		and isMoving):
-			isMoving = false
-			drop()
 			
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if get_rect().has_point(to_local(event.position)):
-			isMoving = true
-			start_pos = position
-
-	if event is InputEventMouseMotion && isMoving:
-		var vector = event.position - start_pos
-		position = start_pos + vector
-
-func set_chess_position(x, y):
-	position.x = x * fieldSize + fieldSize/2
-	position.y = y * fieldSize + fieldSize/2
-
-func get_centered_position(position: Vector2) -> Vector2:
-	var column = int(position.x) / fieldSize
-	var row = int(position.y) / fieldSize
-	if (row < 0 || row > 7 || column < 0 || column > 7):
-		return Vector2.ZERO
+	if can_drag && event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		dragging = true
+		lifted.emit(self)
 		
-	# check if this is a valid move
-	var previousColumn = int(start_pos.x) / fieldSize
-	var previousRow = int(start_pos.y) / fieldSize
-	
-	# move as white pawn
-	if (column != previousColumn):
-		return Vector2.ZERO
-	if (previousRow - 1 != row):
-		return Vector2.ZERO
-	
-	return Vector2(column * fieldSize + fieldSize/2, row * fieldSize + fieldSize/2)
-	
-func drop():
-	var newPosition = get_centered_position(position)
-	if (newPosition == Vector2.ZERO):
-		position = start_pos
-	else:
-		position = newPosition
+	elif dragging && event is InputEventMouseButton and event.is_released and event.button_index == MOUSE_BUTTON_LEFT:
+		dragging = false
+		dropped.emit(self)
+			
+	elif event is InputEventMouseMotion && dragging:
+		position = event.position
+
+func _on_mouse_shape_entered(_shape_idx):
+	if dragging: 
+		return
+	can_drag = true
+
+func _on_mouse_shape_exited(_shape_idx):
+	if dragging: 
+		return
+	can_drag = false
