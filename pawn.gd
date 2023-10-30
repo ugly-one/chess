0adf7d1277175b664c97be1b3d563a56c2a852ea
UglyOne <omojzboze@protonmail.com>
2023-10-30 22:03:01 +0100

    add rock

--- pawn.gd
+++ pawn.gd
@@ -11,14 +11,14 @@ func _ready():
 		$piece.set_texture(black_texture)
 
 func _on_piece_dropped(current_position:Vector2, new_position: Vector2):
-	var can_move = can_move(current_position, new_position, player)
+	var can_move = can_move(current_position, new_position)
 	if (can_move == false):
 		$piece.move(current_position)
 	else:
 		$piece.move(new_position)
 	pass
 
-func can_move(current_position: Vector2, new_position: Vector2, player: Enums.Player) -> bool:
+func can_move(current_position: Vector2, new_position: Vector2) -> bool:
 	if (current_position.y == new_position.y):
 		return false
 	if (new_position.x != current_position.x):
