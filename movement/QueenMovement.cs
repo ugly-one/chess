ce04e72641a8a4960d0f0b06f21400bd8c8e5fb1
UglyOne <omojzboze@protonmail.com>
2023-11-08 14:21:39 +0100

    queen movement - improved

--- movement/QueenMovement.cs
+++ movement/QueenMovement.cs
@@ -13,14 +13,14 @@ public partial class QueenMovement : Movement
     public override Vector2[] GetMoves(Piece[] pieces, Vector2 currentPosition)
     {
         var moves = new List<Vector2>();
-        moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Right));
-        moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Left));
-        moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Left));
-        moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Right));
-        moves.AddRange(currentPosition.GetDirection(Vector2.Up));
-        moves.AddRange(currentPosition.GetDirection(Vector2.Down));
-        moves.AddRange(currentPosition.GetDirection(Vector2.Left));
-        moves.AddRange(currentPosition.GetDirection(Vector2.Right));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Right, pieces, Player));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Left, pieces, Player));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Left, pieces, Player));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Right, pieces, Player));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Up, pieces, Player));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Down, pieces, Player));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Left, pieces, Player));
+        moves.AddRange(currentPosition.GetDirection(Vector2.Right, pieces, Player));
         return moves.ToArray();
     }
 }
