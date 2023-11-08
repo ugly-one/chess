40b68fe6ebb170dafe72c14b1a510af7490828a7
UglyOne <omojzboze@protonmail.com>
2023-11-08 14:20:33 +0100

    bishop movement is also better now

--- movement/BishopMovement.cs
+++ movement/BishopMovement.cs
@@ -13,10 +13,10 @@ public partial class BishopMovement : Movement
 	public override Vector2[] GetMoves(Piece[] pieces, Vector2 currentPosition)
 	{
 		var moves = new List<Vector2>();
-		moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Right));
-		moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Left));
-		moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Left));
-		moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Right));
+		moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Right, pieces, Player));
+		moves.AddRange(currentPosition.GetDirection(Vector2.Up + Vector2.Left, pieces, Player));
+		moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Left, pieces, Player));
+		moves.AddRange(currentPosition.GetDirection(Vector2.Down + Vector2.Right, pieces, Player));
 		return moves.ToArray();
 	}
 }
