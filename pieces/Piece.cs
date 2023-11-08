1b8bbfb123e62e6c6ead54928f2cd52e03f5a19d
UglyOne <omojzboze@protonmail.com>
2023-11-08 15:35:51 +0100

    bla

--- pieces/Piece.cs
+++ pieces/Piece.cs
@@ -2,7 +2,7 @@ using Godot;
 
 public abstract partial class Piece : Node
 {
-	public bool Moved;
+	public bool Moved { get; set; }
 	public Vector2 CurrentPosition { get; set; }
 	public Player Player { get; }
 
