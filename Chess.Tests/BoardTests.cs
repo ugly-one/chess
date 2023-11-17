e9d3c71b7eadd920df7f384b24e9597e1040bc11
UglyOne <omojzboze@protonmail.com>
2023-11-17 04:18:30 +0100

    basic pawn moves tested

--- Chess.Tests/BoardTests.cs
+++ Chess.Tests/BoardTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using Xunit;
 
@@ -16,5 +17,24 @@ public class BoardTests
         var whiteMoves = board.GetPossibleMoves(whiteKing);
         
         Assert.DoesNotContain(whiteMoves, m => m.PieceNewPosition == new Vector2(1,1));
+    }
+    
+    /// <summary>
+    /// A not moved pawn cannot step on a piece in front of it, nor just over it but it can capture another piece
+    /// </summary>
+    [Fact]
+    public void BasicPawnMoves()
+    {
+        var whiteKing = new Piece(PieceType.King, Color.WHITE, new Vector2(3, 3));
+        var blackKing = new Piece(PieceType.King, Color.BLACK, new Vector2(7, 7));
+        var whitePawn = new Piece(PieceType.Pawn, Color.WHITE, new Vector2(3, 4), moved: false);
+        var blackPawn = new Piece(PieceType.Pawn, Color.BLACK, new Vector2(2, 3));
+
+        var board = new Board(new List<Piece>(){ whiteKing, blackKing, whitePawn, blackPawn });
+
+        var moves = board.GetPossibleMoves(whitePawn);
+        Assert.Single(moves);
+        Assert.Equal(moves.First().PieceNewPosition, new Vector2(2,3));
+        Assert.Equal(moves.First().PieceToCapture, blackPawn);
     }
 }
