e4bf94c91c30b79eb7d8a2e885a8847f826a987e
UglyOne <omojzboze@protonmail.com>
2024-07-21 10:02:17 +0200

    depth 4 of performance testing - it's ok, but it takes very long time. The engine is slow.

--- Chess.Tests/PerformanceTests.cs
+++ Chess.Tests/PerformanceTests.cs
@@ -10,31 +10,25 @@ public class PerformanceTests
     {
         { 1, 20 },
         { 2, 400 },
-        { 3, 8902 }
+        { 3, 8902 },
+        { 4, 197281 }
     };
 
-    [Fact]
-    public void Depth1()
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    //[InlineData(4)] -- commented out for now because it takes more than a minute to calculate it
+    public void Depth1(int depth)
     {
         var board = new Board();
-        var count = GetPossibleMovesCount(board, 0, 1, Color.WHITE);
-        Assert.Equal(depthToCount[1], count);
-    }
-
-    [Fact]
-    public void Depth2()
-    {
-        var board = new Board();
-        var count = GetPossibleMovesCount(board, 0, 2, Color.WHITE);
-        Assert.Equal(depthToCount[2] + depthToCount[1], count);
-    }
-
-    [Fact]
-    public void Depth3()
-    {
-        var board = new Board();
-        var count = GetPossibleMovesCount(board, 0, 3, Color.WHITE);
-        Assert.Equal(depthToCount[3] + depthToCount[2] + depthToCount[1], count);
+        var count = GetPossibleMovesCount(board, 0, depth, Color.WHITE);
+        var expectedCount = 0;
+        for (int i = 1; i <= depth; i++)
+        {
+            expectedCount += depthToCount[i];    
+        }
+        Assert.Equal(expectedCount, count);
     }
 
     private int GetPossibleMovesCount(Board board, int currentDepth, int targetDepth, Color color)
