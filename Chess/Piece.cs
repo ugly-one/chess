68631026130ef9e979de549b53a9c534e143556c
UglyOne <omojzboze@protonmail.com>
2024-07-28 21:53:14 +0200

    for some reason (dark theme?!) pieces in terminal are displayed in wrong colors

--- Chess/Piece.cs
+++ Chess/Piece.cs
@@ -14,18 +14,19 @@ public record Piece(PieceType Type, Color Color, Vector Position, bool Moved = f
 	{
 		return (Type, Color) switch
 		{
-			(PieceType.King, Color.WHITE) => "\u2654",
-			(PieceType.King, Color.BLACK) => "\u265a",
-			(PieceType.Queen, Color.WHITE) => "\u2655",
-			(PieceType.Queen, Color.BLACK) => "\u265b",
-			(PieceType.Rock, Color.WHITE) => "\u2656",
-			(PieceType.Rock, Color.BLACK) => "\u265c",
-			(PieceType.Bishop, Color.WHITE) => "\u2657",
-			(PieceType.Bishop, Color.BLACK) => "\u265d",
-			(PieceType.Knight, Color.WHITE) => "\u2658",
-			(PieceType.Knight, Color.BLACK) => "\u265e",
-			(PieceType.Pawn, Color.WHITE) => "\u2659",
-			(PieceType.Pawn, Color.BLACK) => "\u265f",
+            // For some reason I have to switch colors and give white pieces black characters
+			(PieceType.King, Color.BLACK) => "\u2654",
+			(PieceType.King, Color.WHITE) => "\u265a",
+			(PieceType.Queen, Color.BLACK) => "\u2655",
+			(PieceType.Queen, Color.WHITE) => "\u265b",
+			(PieceType.Rock, Color.BLACK) => "\u2656",
+			(PieceType.Rock, Color.WHITE) => "\u265c",
+			(PieceType.Bishop, Color.BLACK) => "\u2657",
+			(PieceType.Bishop, Color.WHITE) => "\u265d",
+			(PieceType.Knight, Color.BLACK) => "\u2658",
+			(PieceType.Knight, Color.WHITE) => "\u265e",
+			(PieceType.Pawn, Color.BLACK) => "\u2659",
+			(PieceType.Pawn, Color.WHITE) => "\u265f",
 			_ => throw new ArgumentOutOfRangeException(),
 		};
 	}
