5dd55b18934902a4422816f8d9d0d3b556e2bd22
Peter Rasmussen <pmrrasmussen@icloud.com>
2024-08-03 07:30:51 +0200

    Change Vector from record struct to struct

--- Chess/Vector.cs
+++ Chess/Vector.cs
@@ -2,8 +2,27 @@ using System;
 
 namespace Chess;
 
-public record struct Vector(int X, int Y)
+public readonly struct Vector(int x, int y)
 {
+    public readonly int X = x;
+
+    public readonly int Y = y;
+
+    public override bool Equals(object? obj)
+        => obj is Vector other && Equals(other);
+
+    public override int GetHashCode()
+        => HashCode.Combine(X, Y);
+
+    public bool Equals(Vector otherSquare)
+        => X == otherSquare.X && Y == otherSquare.Y;
+
+    public static bool operator ==(Vector a, Vector b)
+        => a.Equals(b);
+
+    public static bool operator !=(Vector a, Vector b)
+        => !a.Equals(b);
+
     public static Vector operator +(Vector a, Vector b)
         => new Vector(a.X + b.X, a.Y + b.Y);
 
@@ -18,10 +37,10 @@ public record struct Vector(int X, int Y)
     public static Vector Left => new Vector(-1, 0);
     public static Vector Right => new Vector(1, 0);
 
-    public Vector Abs() 
+    public Vector Abs()
         => new Vector(Math.Abs(X), Math.Abs(Y));
 
-    public Vector Clamp(Vector min, Vector max) 
+    public Vector Clamp(Vector min, Vector max)
         => new Vector(Math.Clamp(X, min.X, max.X), Math.Clamp(Y, min.Y, max.Y));
 
     public Vector Orthogonal()
